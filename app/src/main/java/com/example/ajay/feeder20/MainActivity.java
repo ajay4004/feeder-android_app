4c0413b6c9a008881becfc888fa16b7229c327ca
ajay4004 <ajayyadav4004@gmail.com>
2017-03-19 21:47:07 +0530

    FIrst commit

---
+++ app/src/main/java/com/example/ajay/feeder20/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ajay.feeder20;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    public void Clicklog (View view) {
+        Log.i("clicks","You Clicked B1");
+        Intent i=new Intent(MainActivity.this, Loginpage.class);
+        startActivity(i);
+    }
+
+}
